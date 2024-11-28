ad950657a2a4774d4cc3e2e6b8b0afc108391199
Jonatas D. Assis <jonatasdeassis@gmail.com>
2024-11-28 14:11:11 -0300

    separando componentes

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -4,17 +4,16 @@ import { useForm } from 'react-hook-form'
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import { signUp } from '../../firebase/authentication';
-import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup(){
-    
 
-    const [users, setUsers] = useState([]);
-
+    const navigate = useNavigate();
     const { handleSubmit, register, reset } = useForm();
     
-    async function newUser({email, nome, password}) {
-        const user = await signUp(email, password);
+    async function newUser() {
+        try{ 
+            const user = await signUp(email, password);
         await saveUser({
             email, 
             nome,
@@ -22,62 +21,18 @@ export default function Signup(){
             authId: user.uid
         });
         reset();
-        findUsers();
-        redirect("/login");
-    } 
-
-    async function findUsers() {
-        const users = await getUsers();
-        setUsers(users);
-    }
-
-    async function removeUser(id) {
-        await deleteUser(id)
-        findUsers();
-    }
-
-    async function editUser(id) {
-        const nome = window.prompt('Digite o novo nome:');
-        const email = window.prompt('Digite o novo email:');
-        if (nome && email) {
-            const data = { nome, email };
-            await updateUser(id, data);
-            findUsers();
+        navigate("/login");
+        } catch (error) {
+            alert("Algo deu errado")
+            console.error(error);
         }
-    }
-
-
-    
-    useEffect(() => {
-        findUsers();
-    }, []);
+    } 
 
     return(
         <>
         <Header/>
         <main style={{display: 'flex', flexGrow: 1, justifyContent: 'center'}}>
             <form onSubmit={handleSubmit(newUser)} style={{display: 'flex', flexDirection: 'column', width: '45%', justifyContent: 'center'}}>
-                <table border={"2px"}>
-                    <thead>
-                        <tr>
-                            <td>ID </td>
-                            <td>Nome</td>
-                            <td>Email</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {users.map(user => {
-                        return(
-                        <tr key={user.id}>
-                            <td> {user.id} </td>
-                            <td> {user.nome} </td>
-                            <td> {user?.email} </td>
-                            <td><button type='button' onClick={ () => editUser(user.id)}>Editar</button></td>
-                            <td><button type='button' onClick={ () => removeUser(user.id)}>Excluir</button></td>
-                        </tr>
-                    )})}
-                    </tbody>
-                </table>
                 <label htmlFor='username'>Nome </label>
                 <input type="text" id="username" {...register("nome")} autoComplete="off" />
                 
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,16 +1,69 @@
 import Footer from "../../components/Footer/Footer"
 import UserCard from "../../components/UserCard/UserCard"
 import Header from "../../components/Header/Header"
+import { deleteUser, getUsers, updateUser } from "../../firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../context/Auth";
+import { Navigate } from "react-router-dom";
 
 export default function Users(){
+
+    const { autenticado } = useAuth();
+    if (!autenticado) return <Navigate to='/login' />
+
+    const [users, setUsers] = useState([]);
+
+    async function findUsers() {
+        const users = await getUsers();
+        setUsers(users);
+    }
+
+    async function removeUser(id) {
+        await deleteUser(id)
+        findUsers();
+    }
+
+    async function editUser(id) {
+        const nome = window.prompt('Digite o novo nome:');
+        const email = window.prompt('Digite o novo email:');
+        if (nome && email) {
+            const data = { nome, email };
+            await updateUser(id, data);
+            findUsers();
+        }
+    }
+
+
+    useEffect(() => {
+        findUsers();
+    }, []);
+    
+    
     return (
         <>
             <Header />
             <main style={{flexGrow: 1, textAlign: 'center'}}>
-
-            <h1>Lista de Usuários</h1>
-            <UserCard nome="Jonatas de Assis" idade={16} cargo="Desenvolvedor" />
-            <UserCard nome="Larissa de Assis" idade={17} cargo="Desenvolvedora" />
+            <table border={'2px'} style={{ margin: 'auto auto'}}>
+                    <thead>
+                        <tr>
+                            <td>ID </td>
+                            <td>Nome</td>
+                            <td>Email</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {users.map(user => {
+                        return(
+                        <tr key={user.id}>
+                            <td> {user.id} </td>
+                            <td> {user.nome} </td>
+                            <td> {user?.email} </td>
+                            <td><button type='button' onClick={ () => editUser(user.id)}>Editar</button></td>
+                            <td><button type='button' onClick={ () => removeUser(user.id)}>Excluir</button></td>
+                        </tr>
+                    )})}
+                    </tbody>
+                </table>
             </main>
             <Footer />
            
